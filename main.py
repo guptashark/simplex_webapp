f8db9aa8ec39ff54f2648acd9d2e1068d80f8d99
Aishwarya Grupta <agupta@pyramidsolutions.com>
2021-10-26 21:18:44 -0400

    Create basic working Flask app with Blueprint

---
+++ main.py
@@ -0,0 +1,13 @@
+import flask
+from flask import Blueprint, render_template
+
+simplex_app = Blueprint("simplex_app", __name__, template_folder='templates')
+
+@simplex_app.route("/", methods=["GET"])
+def hello():
+  return "<h1>Simplex App</h1>"
+
+if __name__ == "__main__":
+  app = flask.Flask(__name__)
+  app.register_blueprint(simplex_app, url_prefix="/simplex")
+  app.run(host="localhost", port=8080, debug=True)
